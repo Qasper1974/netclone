f5bfcb615b9366eba03b1160002661e48b7c9e09
Yves Faems <yves.faems@gmail.com>
2022-04-02 20:30:46 -0700

    create Row element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Row from './components/Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
         <h1>START</h1>
+        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
